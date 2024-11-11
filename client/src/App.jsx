433e3ecc1f442679eee7b8d42fdb04081b27100f
Anas <110332632+anasAS02@users.noreply.github.com>
2024-11-11 21:16:28 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,12 @@ import { ToastContainer } from "react-toastify";
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import { StatusProvider } from "./StatusContext";
-import AdminDashboard from "./components/dashboard/admin/AdminDashboard.jsx";
-import ReceptionistDashboard from "./components/dashboard/receptionist/Receptionist.jsx";
-import DoctorDashboard from "./components/dashboard/doctor/DoctorDashboard.jsx";
+import AdminDashboard from "./components/dashboard/admin/AdminDashboard";
+import ReceptionistDashboard from "./components/dashboard/receptionist/Receptionist";
+import DoctorDashboard from "./components/dashboard/doctor/DoctorDashboard";
 import Cookies from 'js-cookie';
-import PharmacistDashboard from "./components/dashboard/pharmacist/PharmacistDashboard.jsx";
-import LaboratoryDoctorDashboard from "./components/dashboard/laboratory-doctor/Laboratory-doctor-Dashboard.jsx";
+import PharmacistDashboard from "./components/dashboard/pharmacist/PharmacistDashboard";
+import LaboratoryDoctorDashboard from "./components/dashboard/laboratory-doctor/Laboratory-doctor-Dashboard";
 
 function App() {
   const role = Cookies.get('role');
