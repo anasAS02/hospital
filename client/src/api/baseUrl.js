3754fee1dcf060b9794fccae0bb76333f5e7d0a4
Anas <110332632+anasAS02@users.noreply.github.com>
2025-05-02 18:24:44 +0300

    as

--- client/src/api/baseUrl.js
+++ client/src/api/baseUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+export const BASE_URL = 'https://hospital-psi-two.vercel.app';
 
 const api = axios.create({ baseURL: BASE_URL });
 
