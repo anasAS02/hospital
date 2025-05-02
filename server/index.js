b56d43d8d568845372ac24041e8e9b6c4d1b6895
Anas <110332632+anasAS02@users.noreply.github.com>
2025-05-02 19:43:21 +0300

    update cors

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ dbConnection();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://hospital-psi-two.vercel.app'],
+  origin: ['http://localhost:5173', 'https://hospital-vercel.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
